1f2649a306fe592859cc7463d1819b24f83c17c6
Peter Heitkemper <71290233+pmh-3@users.noreply.github.com>
2022-01-13 12:59:55 -0800

    Create SumTwoLL.py

---
+++ PYTHON/SumTwoLL.py
@@ -0,0 +1,17 @@
+# Remember: DRY!!
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        addends = l1, l2
+        dummy = end = ListNode(0)
+        carry = 0
+        while addends or carry:
+            carry += sum(a.val for a in addends)
+            addends = [a.next for a in addends if a.next]
+            end.next = end = ListNode(carry % 10)
+            carry //= 10
+        return dummy.next
