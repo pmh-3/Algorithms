51bfb0769b8d4e124e9ee82cc364fd4f560232ec
Peter Heitkemper <71290233+pmh-3@users.noreply.github.com>
2022-02-06 17:37:37 -0800

    Create TwoSum.js

---
+++ TwoSum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function(nums, target) {
+   const comp = {};
+    for(let i=0; i<nums.length; i++){
+        if(comp[nums[i] ]>=0){
+            return [ comp[nums[i] ] , i]
+        }
+        comp[target-nums[i]] = i
+    }
+};
